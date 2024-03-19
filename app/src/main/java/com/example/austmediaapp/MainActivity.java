617a5394432fe19949d5e49f9ba557e0c63778f3
Joe Al Sokhen <jya20008@students.aust.edu.lb>
2024-03-19 18:49:35 +0200

    Media: Add list view functionality & improve logic of media controls
    Signed-off-by: Joe Al Sokhen <jya20008@students.aust.edu.lb>

--- app/src/main/java/com/example/austmediaapp/MainActivity.java
+++ app/src/main/java/com/example/austmediaapp/MainActivity.java
@@ -5,13 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     Button play, pause, stop;
     MediaPlayer mp;
+    ListView mediaList;
     int flag =1;
+    List <String> mediaFiles;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,42 +29,85 @@ public class MainActivity extends AppCompatActivity {
         play = findViewById(R.id.btnPlay);
         pause = findViewById(R.id.btnPause);
         stop = findViewById(R.id.btnStop);
+        mediaList = findViewById(R.id.lvMedia);
 
-        mp = MediaPlayer.create(this, R.raw.sample_getyoutothemoon);
+
+        mediaFiles = getRawResourceNames();
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.list_view_media_item, mediaFiles);
+        mediaList.setAdapter(adapter);
+
+        mediaList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                playMedia(position);
+            }
+    });
+
         play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                if (flag ==0)
-                    mp = MediaPlayer.create(MainActivity.this, R.raw.sample_getyoutothemoon);
-
-                mp.start();
-                play.setEnabled(false);
-                pause.setEnabled(true);
-                stop.setEnabled(true);
-                flag = 1;
+                if (flag == 0 || mp == null) {
+                    // On cold app boot, if play button is pressed: play default position
+                    playMedia(0);
+                    flag = 1;
+                } else {
+                    mp.start();
+                    updateButtonStates(false, true,true);
+                }
             }
         });
 
         pause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mp.pause();
-                pause.setEnabled(false);
-                play.setEnabled(true);
+                if (mp != null && mp.isPlaying()) {
+                    mp.pause();
+                    updateButtonStates(true, false, false);
+                }
+
             }
         });
 
         stop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mp.stop();
-                stop.setEnabled(false);
-                pause.setEnabled(false);
-                play.setEnabled(true);
+                stopMedia();
+                updateButtonStates(true, false, false);
                 flag = 0;
             }
         });
 
+    }
+    private void playMedia(int position) {
+        stopMedia();
+
+        int resId = getResources().getIdentifier(mediaFiles.get(position), "raw", getPackageName());
+        mp = MediaPlayer.create(this, resId);
+        mp.start();
+        updateButtonStates(false, true, true);
+        flag = 1;
+    }
+
+    private void stopMedia() {
+        if (mp != null) {
+            mp.stop();
+            mp.release();
+            mp = null;
+            flag = 0;
+        }
+    }
+
+    private void updateButtonStates(boolean playEnabled, boolean pauseEnabled, boolean stopEnabled) {
+        play.setEnabled(playEnabled);
+        pause.setEnabled(pauseEnabled);
+        stop.setEnabled(stopEnabled);
+    }
+    private List<String> getRawResourceNames() {
+        List<String> rawList = new ArrayList<>();
+        Field[] fields = R.raw.class.getFields();
+        for (Field field : fields) {
+            rawList.add(field.getName());
+        }
+        return rawList;
     }
 }
